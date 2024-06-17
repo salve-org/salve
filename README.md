604ca32dfaff1f0ed7f1a67d63962ee5f9b52aeb
Moosems <95927277+Moosems@users.noreply.github.com>
2024-06-16 20:11:48 -0600

    Bump version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 align="center">Salve v0.5.1</h1>
+<h1 align="center">Salve v0.5.2</h1>
 
 # Installation
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="salve_ipc",
-    version="0.5.1",
+    version="0.5.2",
     description="Salve is an IPC library that can be used by code editors to easily get autocompletions, replacements, editorconfig suggestions, definitions, and syntax highlighting.",
     author="Moosems",
     author_email="moosems.j@gmail.com",
