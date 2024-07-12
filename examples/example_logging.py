75f631777fa5973fdea0a6539b041adea8c13312
Moosems <95927277+Moosems@users.noreply.github.com>
2024-07-12 15:57:24 -0600

    Add most of the logging and fix setup.py
    I still need to give logging to the Tree Sitter part

---
+++ examples/example_logging.py
@@ -0,0 +1,34 @@
+from time import sleep
+
+from salve import HIGHLIGHT, IPC, Response
+from logging import Logger, basicConfig, INFO, getLogger
+
+basicConfig(
+    level=INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger: Logger = getLogger("Main")
+
+def main():
+    context = IPC()
+
+    context.update_file(
+        "test",
+        open(__file__, "r+").read(),
+    )
+
+    context.request(
+        HIGHLIGHT, file="test", language="python", text_range=(1, 30)
+    )
+
+    sleep(1)
+    output: Response | None = context.get_response(HIGHLIGHT)
+    if output is None:
+        logger.info("Output is None")
+
+    logger.info(f"Output: {output}")
+
+    context.kill_IPC()
+
+
+if __name__ == "__main__":
+    main()
--- salve/server.py
+++ salve/server.py
@@ -1,3 +1,4 @@
+from logging import Logger
 from multiprocessing.queues import Queue as GenericClassQueue
 from pathlib import Path
 from time import sleep
@@ -32,7 +33,10 @@ class Server:
         self,
         response_queue: GenericClassQueue,
         requests_queue: GenericClassQueue,
+        logger: Logger,
     ) -> None:
+        self.logger: Logger = logger
+        self.logger.info("Starting server setup")
         self.response_queue: ResponseQueueType = response_queue
         self.requests_queue: RequestQueueType = requests_queue
         self.all_ids: list[int] = []
@@ -44,38 +48,54 @@ class Server:
 
         self.files: dict[str, str] = {}
 
+        self.logger.info("Server setup complete")
+
         while True:
             self.run_tasks()
             sleep(0.0025)
 
     def simple_id_response(self, id: int, cancelled: bool = True) -> None:
+        self.logger.debug(f"Creating simple response for id {id}")
         response: Response = {
             "id": id,
             "type": "response",
             "cancelled": cancelled,
         }
+        self.logger.debug(f"Sending simple response for id {id}")
         self.response_queue.put(response)
+        self.logger.info(f"Simple response for id {id} sent")
 
     def parse_line(self, message: Request | Notification) -> None:
+        self.logger.debug("Parsing Message from user")
         id: int = message["id"]
         match message["type"]:
             case "notification":
+                self.logger.debug("Mesage is of type notification")
                 file: str = message["file"]  # type: ignore
                 if message["remove"]:  # type: ignore
+                    self.logger.info(f"File {file} was requested for removal")
                     self.files.pop(file)
+                    self.logger.info(f"File {file} has been removed")
                     return
                 contents: str = message["contents"]  # type: ignore
                 self.files[file] = contents
+                self.logger.info(f"File {file} has been updated with new contents")
                 self.simple_id_response(id, False)
+                self.logger.debug(f"Notification response for id {id} has been sent")
             case "request":
+                self.logger.info(f"Mesage with id {id} is of type request")
                 self.all_ids.append(id)
                 command: str = message["command"]  # type: ignore
                 self.newest_ids[command] = id
                 self.newest_requests[command] = message  # type: ignore
+                self.logger.debug(f"Request stored for parsing")
             case _:
+                self.logger.warning(f"Unknown type {type}. Sending simple response")
                 self.simple_id_response(id)
+                self.logger.debug(f"Simple response for id {id} sent")
 
     def cancel_all_ids_except_newest(self) -> None:
+        self.logger.info("Cancelling all old id's")
         ids = [
             id["id"]
             for id in list(self.newest_requests.values())
@@ -83,10 +103,13 @@ class Server:
         ]
         for id in self.all_ids:
             if id in ids:
+                self.logger.debug(f"Id {id} is newest of its command")
                 continue
+            self.logger.debug(f"Id {id} is an old request, sending simple respone")
             self.simple_id_response(id)
 
         self.all_ids = []
+        self.logger.debug("All ids list reset")
 
     def handle_request(self, request: Request) -> None:
         command: str = request["command"]
@@ -99,18 +122,21 @@ class Server:
 
         match request["command"]:
             case "autocomplete":
+                self.logger.info("Finding completions for request")
                 result = find_autocompletions(
                     full_text=self.files[file],
                     expected_keywords=request["expected_keywords"],  # type: ignore
                     current_word=request["current_word"],  # type: ignore
                 )
             case "replacements":
+                self.logger.info("Getting replacements for request")
                 result = get_replacements(
                     full_text=self.files[file],
                     expected_keywords=request["expected_keywords"],  # type: ignore
                     replaceable_word=request["current_word"],  # type: ignore
                 )
             case "highlight":
+                self.logger.info("Getting normal highlights for request")
                 pre_refined_result: list[Token] = get_highlights(
                     full_text=self.files[file],
                     language=request["language"],  # type: ignore
@@ -118,30 +144,45 @@ class Server:
                 )
                 result += [token for token in pre_refined_result]  # type: ignore
             case "editorconfig":
+                self.logger.info("Getting editorconfig info for request")
                 result = get_config(request["file_path"])  # type: ignore
             case "definition":
+                self.logger.info("Getting definition for request")
                 result = get_definition(
                     self.files[file],
                     request["definition_starters"],  # type: ignore
                     request["current_word"],  # type: ignore
                 )
             case "highlight-tree-sitter":
+                self.logger.info("Getting Tree Sitter highlights for request")
+
+                self.logger.debug("Getting language function")
                 language_function: Callable[[], int] | Path | str = request[
                     "tree_sitter_language"
                 ]  # type: ignore
                 if isinstance(language_function, Path):
+                    self.logger.info("Language function is pathlib.Path")
                     lang = lang_from_so(
                         str(language_function.absolute()),
                         request["language"],  # type: ignore
                     )
+                    self.logger.debug("Language created")
                 elif isinstance(language_function, str):
+                    self.logger.info("Language function is str")
                     lang = lang_from_so(
                         language_function,
                         request["language"],  # type: ignore
                     )  # type: ignore
+                    self.logger.debug("Language created")
                 elif callable(language_function):
+                    self.logger.info("Language function is actual function")
                     lang = Language(language_function())
+                    self.logger.debug("Language created")
+
+                self.logger.debug("Creating Parser")
                 parser = Parser(lang)
+                self.logger.debug("Getting highlights from parser")
+                # TODO: give logger to tree_sitter_highlight
                 result = tree_sitter_highlight(  # type: ignore
                     self.files[file],
                     request["language"],  # type: ignore
@@ -151,7 +192,7 @@ class Server:
                 )
 
             case _:
-                print("NOT RECOGNIZED", command)
+                self.logger.warning(f"Command {command} not recognized")
                 cancelled = True
 
         response: Response = {
@@ -161,24 +202,32 @@ class Server:
             "command": command,
             "result": result,
         }
+        self.logger.debug("Response created")
         self.response_queue.put(response)
         self.newest_ids[command] = 0
+        self.logger.info(f"Response sent forf request of command {command}")
 
     def run_tasks(self) -> None:
+        if  self.requests_queue.empty():
+            return
+
+        self.logger.debug("New request in queue")
         while not self.requests_queue.empty():
+            self.logger.debug("Parsing request")
             self.parse_line(self.requests_queue.get())
 
-        self.cancel_all_ids_except_newest()
+        if not self.all_ids:
+            self.logger.debug("All requests were notifications")
 
-        if not list(
-            self.newest_requests.values()
-        ):  # There may have only been refreshes
-            return
+        self.logger.debug("Cancelling all old id's")
+        self.cancel_all_ids_except_newest()
 
         # Actual work
         for request in list(self.newest_requests.values()):
             if request is None:
                 continue
-            self.handle_request(request)
             command: str = request["command"]
+            self.logger.info(f"Handling request of command {command}")
+            self.handle_request(request)
             self.newest_requests[command] = None
+            self.logger.debug("Request completed")
