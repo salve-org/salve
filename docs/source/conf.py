1426c1561d2cb24d7a1445c4860cee0f4d8423be
Moosems <95927277+Moosems@users.noreply.github.com>
2024-07-11 21:20:55 -0600

    Add mkdocs and format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../examples'))
+
+sys.path.insert(0, os.path.abspath("../examples"))
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -10,23 +11,22 @@ sys.path.insert(0, os.path.abspath('../examples'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Salve'
-copyright = '2024, Moosems'
-author = 'Moosems'
-release = 'v0.7.1'
+project = "Salve"
+copyright = "2024, Moosems"
+author = "Moosems"
+release = "v0.7.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
