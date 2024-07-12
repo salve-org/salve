196b14e3d32fa0811023bde37ded814d03d97327
Moosems <95927277+Moosems@users.noreply.github.com>
2024-07-12 12:56:49 -0600

    Bump docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../examples"))
 project = "Salve"
 copyright = "2024, Moosems"
 author = "Moosems"
-release = "v0.7.1"
+release = "v0.7.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
