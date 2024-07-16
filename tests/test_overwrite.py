fc06d82cb78192c74847c0aea5eed7a92641f0e7
Moosems <95927277+Moosems@users.noreply.github.com>
2024-07-16 14:04:40 -0600

    Format

--- tests/test_overwrite.py
+++ tests/test_overwrite.py
@@ -1,22 +1,31 @@
 from salve.server_functions.highlight.tokens import overwrite_tokens
 
+
 def test_overwrite_tokens():
-    assert overwrite_tokens([((1, 0), 3, "TEST1")], [((1, 2), 3, "TEST2")]) == [
+    assert overwrite_tokens(
+        [((1, 0), 3, "TEST1")], [((1, 2), 3, "TEST2")]
+    ) == [
         ((1, 0), 2, "TEST1"),
         ((1, 2), 3, "TEST2"),
     ]
-    assert overwrite_tokens([((1, 0), 1, "TEST1")], [((1, 2), 3, "TEST2")]) == [
+    assert overwrite_tokens(
+        [((1, 0), 1, "TEST1")], [((1, 2), 3, "TEST2")]
+    ) == [
         ((1, 0), 1, "TEST1"),
         ((1, 2), 3, "TEST2"),
     ]
-    assert overwrite_tokens([((1, 4), 5, "TEST1")], [((1, 2), 3, "TEST2")]) == [
+    assert overwrite_tokens(
+        [((1, 4), 5, "TEST1")], [((1, 2), 3, "TEST2")]
+    ) == [
         ((1, 2), 3, "TEST2"),
         ((1, 5), 4, "TEST1"),
     ]
-    assert overwrite_tokens([((1, 3), 1, "TEST1")], [((1, 2), 3, "TEST2")]) == [
-        ((1, 2), 3, "TEST2")
-    ]
-    assert overwrite_tokens([((1, 0), 9, "TEST1")], [((1, 2), 3, "TEST2")]) == [
+    assert overwrite_tokens(
+        [((1, 3), 1, "TEST1")], [((1, 2), 3, "TEST2")]
+    ) == [((1, 2), 3, "TEST2")]
+    assert overwrite_tokens(
+        [((1, 0), 9, "TEST1")], [((1, 2), 3, "TEST2")]
+    ) == [
         ((1, 0), 2, "TEST1"),
         ((1, 2), 3, "TEST2"),
         ((1, 5), 4, "TEST1"),
